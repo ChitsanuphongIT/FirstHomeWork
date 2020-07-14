d3c3fafb8de85d119293be83a8d81a45b55efec4
ChitsanuphongIT <67995734+ChitsanuphongIT@users.noreply.github.com>
2020-07-14 22:46:32 +0700

    Add files via upload

---
+++ ILOVEYOU3000.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std ;
+int main() 
+{
+
+	cout << "================================================================================  " << endl ;
+	cout << "1st Term of 2020" << "   630602160039" << "   Yada Sarasomsaeng" << endl ;
+	cout << "  " << endl ;
+	cout << "--------------------------------------------------------------------------------" << endl ;
+	cout << "| SUBJECT ID | SUBJECT NAME                               | SCORE (100) | GRADE | " << endl ;
+	cout << "--------------------------------------------------------------------------------" << endl ;
+	cout << "| 080103001  | English I                                  |      89     |   A   | " << endl ;
+	cout << "| 040203100  | General Mathematics                        |      91     |   A   | " << endl ;
+	cout << "| 060243102  | Computer Programming                       |      87     |   A   | " << endl ;
+	cout << "| 060243103  | Problem Solving In Information Technology  |      85     |   A   | " << endl ;
+	cout << "| 080303606  | Systematic And Creative Thinkking          |      81     |   A   | " << endl ;
+	cout << "| 060243101  | Fundamental Of Information Technnology     |      84     |   A   | " << endl ;
+	cout << "| 080303518  | Aerobic Dance And Cover Dance              |      86     |   A   | " << endl ;
+	cout << "--------------------------------------------------------------------------------" << endl ;
+	cout << "|    TOTAL   | SUBJECT = 7                                |    AVERAGE = 4.00   | " << endl ;
+	cout << "================================================================================  " << endl ;
+	cout << " " << endl ;
+	cout << " " << endl ;
+	cout << " " << endl ;
+	cout << "          KING MONGKUT'S UNIVERSITY OF TECHNOLOGY NORTH BANGKOK - KMUTNB           " << endl ;
+
+	 system("pause");
+     return (0) ;
+}
